610d19ac8115be4a4b89bde680105a49ad902ad7
Pedro Henrique <pedrohenriquepaula1007@gmail.com>
2026-04-13 06:36:45 -0300

    (Atualização 12_04_26)

--- coletor.py
+++ coletor.py
@@ -179,4 +179,4 @@ def coletar_por_lotes(data_inicio_str, data_fim_str):
             time.sleep(0.5)
 
 if __name__ == "__main__":
-    coletar_por_lotes("20260401", "20260404")
+    coletar_por_lotes("20260409", "20260412")
